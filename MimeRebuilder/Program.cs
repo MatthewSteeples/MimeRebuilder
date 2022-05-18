848d953aaeffade5b087e712f41486ab4412bdf3
Matthew Steeples <matthew@ledgerscope.com>
2022-05-18 17:15:25 +0100

    Add project files.

---
+++ MimeRebuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MimeRebuilder;
+
+namespace MimeTypesFull
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MimeTypes.TryGetMimeType(args[0], out var mimeType);
+		}
+	}
+}
